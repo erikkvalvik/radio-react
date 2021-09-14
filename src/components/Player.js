d705bd785a9abefd8e1f11279a0dd8d9cc68f899
erikkvalvik <erik.kvalvik@gmail.com>
2021-09-14 16:46:52 +0200

    fixed time stamps

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react/cjs/react.development';
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     //Ref
@@ -15,14 +16,29 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             setIsPlaying(!isPlaying);
         }
     }
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration: duration})
+    }
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
+    };
+    //State
+    const [songInfo, setSongInfo] = useState({
+        currentTime: null,
+        duration: null,
+    })
 
 
     return(
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range" />
-                <p>End Time</p>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
@@ -30,7 +46,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
 
             </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
+            <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
