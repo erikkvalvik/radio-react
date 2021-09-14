36ca392472c88c80b24ba6427387b0e05f6c1221
erikkvalvik <erik.kvalvik@gmail.com>
2021-09-14 16:26:16 +0200

    adding Ref

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import data from "./util";
 function App() {
   //State
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [currentSong, setCurrentSong] = useState(songs[1]);
 
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
