e74ed546ff9c3a351a7088d389191eeb62c96d2c
erikkvalvik <erik.kvalvik@gmail.com>
2021-09-15 18:20:48 +0200

    implemented song library component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 //Import Styles
 import './styles/app.scss';
+
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
 //Import Util
 import data from "./util";
 
@@ -17,7 +19,12 @@ function App() {
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
+      <Player 
+        isPlaying={isPlaying} 
+        setIsPlaying={setIsPlaying} 
+        currentSong={currentSong} 
+      />
+      <Library songs={songs}  />
     </div>
   );
 }
